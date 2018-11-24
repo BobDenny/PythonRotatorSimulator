269d9a44708ca5b3a51ab215a6e28aa2ec3e3fe9
Bob Denny <rdenny@dc3.com>
2018-11-24 12:12:58 -0700

    Gaininig on it. Much of the Swagger stuff is showing OK. Now need to enable PUT from Swagger UI.

--- Rotator/app.py
+++ Rotator/app.py
@@ -11,11 +11,11 @@ from flask_restplus import Api, Resource, fields
 #
 app = Flask(__name__)
 blueprint = Blueprint('Rotator', __name__, url_prefix='/API/V1/Rotator')   # Create a URL prefix for the whole application
-#api = Api(validate=True, doc='/documentation')          # Validates input to Swagger documentation and moves Swagger off root
 api = Api()
 api.init_app(blueprint, 
-                title='ASCOM Rotator API', 
-                description='This implements a demo of the ASCOM Rest API for Rotator')
+                version = '1.0',
+                title='ASCOM Rotator Simulator API', 
+                description='This device is an ASCOM Rotator simulator that responds to the standard REST API for Rotator')
 app.register_blueprint(blueprint)
 
 
@@ -41,26 +41,33 @@ svrtransid = 0                                           # Counts up
 # Models and Wrapper Classes
 # ==========================
 
+m_CommonRequest = api.model('CommonRequest',
+                                {   'DeviceNumber'           : fields.Integer(0, description='Zero-based device number as set on the server', required=True),
+                                    'ClientID'               : fields.Integer(1, description='Client\'s unique ID'),
+                                    'ClientTransactionID'    : fields.Integer(1234, description='Client\'s transaction ID'),
+                                })
 # ------------
 # BoolResponse
 # ------------
 class BoolResponse(dict):
     def __init__(self, value, method, clitransid):
+        global svrtransid
+        svrtransid += 1
         self.Value = value
         self.ClientTransactionIDForm = 0
-        self.ServerTransactionID = 0
+        self.ServerTransactionID = svrtransid
         self.Method = method
         self.ErrorNumber = 0
         self.ErrorMessage = ''
         self.DriverException = None
 
 m_BoolResponse = api.model('BoolResponse', 
-                                {   'Value'                   : fields.Boolean('True or False value.'),
-                                    'ClientTransactionIDForm' : fields.Integer('Client\'s transaction ID.'),
-                                    'ServerTransactionID'     : fields.Integer('Server\s transaction ID.'),
-                                    'Method'                  : fields.String('Name of the calling method.'),
-                                    'ErrorNumber'             : fields.Integer('Error number from device.'),
-                                    'ErrorMessage'            : fields.String('Error message description from device.')
+                                {   'Value'                   : fields.Boolean(description='True or False value.', required=True),
+                                    'ClientTransactionIDForm' : fields.Integer(description='Client\'s transaction ID.'),
+                                    'ServerTransactionID'     : fields.Integer(description='Server\'s transaction ID.'),
+                                    'Method'                  : fields.String(description='Name of the calling method.'),
+                                    'ErrorNumber'             : fields.Integer(description='Error number from device.'),
+                                    'ErrorMessage'            : fields.String(description='Error message description from device.')
                                 })
 
 # --------------
@@ -68,32 +75,37 @@ m_BoolResponse = api.model('BoolResponse',
 # --------------
 class MethodResponse(dict):
     def __init__(self, method, clitransid):
+        global svrtransid
+        svrtransid += 1
         self.ClientTransactionIDForm = 0
-        self.ServerTransactionID = 0
+        self.ServerTransactionID = svrtransid
         self.Method = method
         self.ErrorNumber = 0
         self.ErrorMessage = ''
         self.DriverException = None
 
 m_MethodResponse = api.model('MethodResponse', 
-                                {   'ClientTransactionIDForm' : fields.Integer('Client\'s transaction ID.'),
-                                    'ServerTransactionID'     : fields.Integer('Server\s transaction ID.'),
-                                    'Method'                  : fields.String('Name of the calling method.'),
-                                    'ErrorNumber'             : fields.Integer('Error number from device.'),
-                                    'ErrorMessage'            : fields.String('Error message description from device.')
+                                {   'ClientTransactionIDForm' : fields.Integer(description='Client\'s transaction ID.'),
+                                    'ServerTransactionID'     : fields.Integer(description='Server\'s transaction ID.'),
+                                    'Method'                  : fields.String(description='Name of the calling method.'),
+                                    'ErrorNumber'             : fields.Integer(description='Error number from device.'),
+                                    'ErrorMessage'            : fields.String(description='Error message description from device.')
                                })
 
 # ==============================
 # API ENDPOINTS (REST Resources)
 # ==============================
 
-# --------
-# language - List ([]) of 'Language' per the model above 
-# --------
-#
-@api.route('/<int:DeviceNumber>/CanReverse') 
+@api.route('/<int:DeviceNumber>/CanReverse', methods=['GET']) 
+@api.param('DeviceNumber', 'Zero-based device number as set on the server', 'path', type='integer', default='0')
+@api.param('ClientID', 'Client\'s unique ID', 'query', type='integer', default='1234')
+@api.param('ClientTransactionID', 'Client\'s transaction ID', 'query', type='integer', default='1')
+##@api.doc(description='This is a class test') Probably shows in all methods
 class CanReverse(Resource):
 
+    @api.doc(description='True if the Rotator supports the <b>Reverse</b> method')
+    @api.marshal_with(m_BoolResponse, description='Driver response')
+    #@api.expect(m_CommonRequest)
     def get(self, DeviceNumber):
         devno = DeviceNumber                    # Used later for multi-device (typ.)
         cid = request.args.get('ClientID', 1)
@@ -101,21 +113,33 @@ class CanReverse(Resource):
         return vars(R)
 
 
+# -------
+# Reverse
+# -------
+#
 @api.route('/<int:DeviceNumber>/Reverse', methods=['GET','PUT'])
+@api.param('DeviceNumber', 'Zero-based device number as set on the server', 'path', type='integer', default='0')
+@api.param('ClientID', 'Client\'s unique ID', 'query', type='integer', default='1234')
+@api.param('ClientTransactionID', 'Client\'s transaction ID', 'query', type='integer', default='1')
 class Reverse(Resource):
 
+    @api.doc(description='Returns the Rotator\'s <b>Reverse</b> state')
+    @api.marshal_with(m_BoolResponse, description='Driver response')
     def get(self, DeviceNumber):
         devno = DeviceNumber
         cid = request.args.get('ClientID', 1)
         R = BoolResponse(reverse, request.method, request.args.get('ClientTransactionID', 1234))
         return vars(R)
 
-    def post(self, DeviceNumber):
+    @api.doc(description='Sets the Rotator\'s <b>Reverse</b> state')
+    @api.marshal_with(m_MethodResponse, description='Driver response')
+    def put(self, DeviceNumber):
         devno = DeviceNumber
         cid = request.args.get('ClientID', 1)
         reverse = api.payload['Reverse']
         R = MethodResponse(request.method, request.args.get('ClientTransactionID', 1234))
         return vars(R)
+
 
 # ==================
 # SERVER APPLICATION
@@ -126,10 +150,11 @@ class Reverse(Resource):
 #
 if __name__ == '__main__':
 
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    #HOST = os.environ.get('SERVER_HOST', 'localhost')
 
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    #try:
+    #    PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    #except ValueError:
+    #    PORT = 5555
+    #app.run(HOST, PORT, debug=True)
+    app.run('127.0.0.1', 5555, debug=True)
