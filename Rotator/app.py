6e13023d83e83ff2f655ef05cd898cb5ba4ce63a
Bob Denny <rdenny@dc3.com>
2018-11-23 19:33:30 -0700

    Running more or less, the server transid is not incrementing

--- Rotator/app.py
+++ Rotator/app.py
@@ -1,23 +1,133 @@
-"""
-This script runs the application using a development server.
-It contains the definition of routes and views for the application.
-"""
+# https://ascom-standards.org/api
+# https://exploreflask.com/en/latest/views.html#url-converters
 
-from flask import Flask
+import os
+from flask import Flask, Blueprint, request
+from flask_restplus import Api, Resource, fields
+
+# ===============================
+# FLASK SERVER AND REST FRAMEWORK
+# ===============================
+#
 app = Flask(__name__)
+blueprint = Blueprint('Rotator', __name__, url_prefix='/API/V1/Rotator')   # Create a URL prefix for the whole application
+#api = Api(validate=True, doc='/documentation')          # Validates input to Swagger documentation and moves Swagger off root
+api = Api()
+api.init_app(blueprint, 
+                title='ASCOM Rotator API', 
+                description='This implements a demo of the ASCOM Rest API for Rotator')
+app.register_blueprint(blueprint)
 
-# Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
+
+# =========
+# App State
+# =========
+#
+# Including initialization (default contents)
+# 
+# State
+#
+position = 0.0 
+can_reverse = True
+reverse = False
+step_size = 0.1
+target_position = 0.0
+#
+# Connection
+#
+svrtransid = 0                                           # Counts up
+
+# ==========================
+# Models and Wrapper Classes
+# ==========================
+
+# ------------
+# BoolResponse
+# ------------
+class BoolResponse(dict):
+    def __init__(self, value, method, clitransid):
+        self.Value = value
+        self.ClientTransactionIDForm = 0
+        self.ServerTransactionID = 0
+        self.Method = method
+        self.ErrorNumber = 0
+        self.ErrorMessage = ''
+        self.DriverException = None
+
+m_BoolResponse = api.model('BoolResponse', 
+                                {   'Value'                   : fields.Boolean('True or False value.'),
+                                    'ClientTransactionIDForm' : fields.Integer('Client\'s transaction ID.'),
+                                    'ServerTransactionID'     : fields.Integer('Server\s transaction ID.'),
+                                    'Method'                  : fields.String('Name of the calling method.'),
+                                    'ErrorNumber'             : fields.Integer('Error number from device.'),
+                                    'ErrorMessage'            : fields.String('Error message description from device.')
+                                })
+
+# --------------
+# MethodResponse
+# --------------
+class MethodResponse(dict):
+    def __init__(self, method, clitransid):
+        self.ClientTransactionIDForm = 0
+        self.ServerTransactionID = 0
+        self.Method = method
+        self.ErrorNumber = 0
+        self.ErrorMessage = ''
+        self.DriverException = None
+
+m_MethodResponse = api.model('MethodResponse', 
+                                {   'ClientTransactionIDForm' : fields.Integer('Client\'s transaction ID.'),
+                                    'ServerTransactionID'     : fields.Integer('Server\s transaction ID.'),
+                                    'Method'                  : fields.String('Name of the calling method.'),
+                                    'ErrorNumber'             : fields.Integer('Error number from device.'),
+                                    'ErrorMessage'            : fields.String('Error message description from device.')
+                               })
+
+# ==============================
+# API ENDPOINTS (REST Resources)
+# ==============================
+
+# --------
+# language - List ([]) of 'Language' per the model above 
+# --------
+#
+@api.route('/<int:DeviceNumber>/CanReverse') 
+class CanReverse(Resource):
+
+    def get(self, DeviceNumber):
+        devno = DeviceNumber                    # Used later for multi-device (typ.)
+        cid = request.args.get('ClientID', 1)
+        R = BoolResponse(can_reverse, request.method, request.args.get('ClientTransactionID', 1234))
+        return vars(R)
 
 
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+@api.route('/<int:DeviceNumber>/Reverse', methods=['GET','PUT'])
+class Reverse(Resource):
 
+    def get(self, DeviceNumber):
+        devno = DeviceNumber
+        cid = request.args.get('ClientID', 1)
+        R = BoolResponse(reverse, request.method, request.args.get('ClientTransactionID', 1234))
+        return vars(R)
+
+    def post(self, DeviceNumber):
+        devno = DeviceNumber
+        cid = request.args.get('ClientID', 1)
+        reverse = api.payload['Reverse']
+        R = MethodResponse(request.method, request.args.get('ClientTransactionID', 1234))
+        return vars(R)
+
+# ==================
+# SERVER APPLICATION
+# ==================
+#
+# Start it this way to get  the automatic tab on Chrome
+# with the right host/port.
+#
 if __name__ == '__main__':
-    import os
+
     HOST = os.environ.get('SERVER_HOST', 'localhost')
+
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
---
+++ TestBed/TestBed.py
@@ -0,0 +1,10 @@
+class A(dict):
+    def __init__(self):
+        self.b = 1
+        self.c = 2
+    def do_nothing(self):
+        pass
+
+a = A()
+print(a)
+
